4389f1418792632e7faede1b18ce3ebd81f9640c
Houssam Habbat <houssam.habbat@terabyte-software.com>
2024-01-22 17:27:24 +0100

    fix: footer test

--- src/tests/layout/footer.test.tsx
+++ src/tests/layout/footer.test.tsx
@@ -1,10 +1,11 @@
 import { Footer } from '@/components/layout/UI';
 import { render, screen } from '@/configs/test-utils';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 describe('test <Footer/>', () => {
+  const mockfn = vi.fn(() => {});
   it('should render correctly', async () => {
-    render(<Footer />);
+    render(<Footer setFooterStatus={mockfn} footerStatus={'expanded'} newsList={[]} />);
     expect(
       screen.getByRole('heading', {
         name: /latest news/i,
